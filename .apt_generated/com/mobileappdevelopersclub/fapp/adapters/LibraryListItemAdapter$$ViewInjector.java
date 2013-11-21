338f0542c15e78e629c88b1fdb08b361d769a65e
Vinnie Vendemia <vvendemia1@gmail.com>
2013-11-20 19:06:20 -0500

    making changes to the schedule fragment, adding new features to the library list

--- .apt_generated/com/mobileappdevelopersclub/fapp/adapters/LibraryListItemAdapter$$ViewInjector.java
+++ .apt_generated/com/mobileappdevelopersclub/fapp/adapters/LibraryListItemAdapter$$ViewInjector.java
@@ -7,11 +7,11 @@ import butterknife.Views.Finder;
 public class LibraryListItemAdapter$$ViewInjector {
   public static void inject(Finder finder, final com.mobileappdevelopersclub.fapp.adapters.LibraryListItemAdapter target, Object source) {
     View view;
-    view = finder.findById(source, 2131296275);
+    view = finder.findById(source, 2131296274);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296275' for field 'libraryHours' was not found. If this field binding is optional add '@Optional'.");
+      throw new IllegalStateException("Required view with id '2131296274' for field 'libraryLocation' was not found. If this field binding is optional add '@Optional'.");
     }
-    target.libraryHours = (android.widget.TextView) view;
+    target.libraryLocation = (android.widget.TextView) view;
     view = finder.findById(source, 2131296273);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131296273' for field 'libraryName' was not found. If this field binding is optional add '@Optional'.");
@@ -22,17 +22,17 @@ public class LibraryListItemAdapter$$ViewInjector {
       throw new IllegalStateException("Required view with id '2131296272' for field 'libraryImage' was not found. If this field binding is optional add '@Optional'.");
     }
     target.libraryImage = (android.widget.ImageView) view;
-    view = finder.findById(source, 2131296274);
+    view = finder.findById(source, 2131296275);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296274' for field 'libraryLocation' was not found. If this field binding is optional add '@Optional'.");
+      throw new IllegalStateException("Required view with id '2131296275' for field 'libraryHours' was not found. If this field binding is optional add '@Optional'.");
     }
-    target.libraryLocation = (android.widget.TextView) view;
+    target.libraryHours = (android.widget.TextView) view;
   }
 
   public static void reset(com.mobileappdevelopersclub.fapp.adapters.LibraryListItemAdapter target) {
-    target.libraryHours = null;
+    target.libraryLocation = null;
     target.libraryName = null;
     target.libraryImage = null;
-    target.libraryLocation = null;
+    target.libraryHours = null;
   }
 }
