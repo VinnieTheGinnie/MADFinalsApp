cb9be982f9dec0f736366a5ca61e2f53640e8d7e
Vinnie Vendemia <vvendemia1@gmail.com>
2013-12-01 16:20:16 -0500

    cleaned up ui on schedule fragment

--- .apt_generated/com/mobileappdevelopersclub/fapp/adapters/FinalsResponseAdapter$$ViewInjector.java
+++ .apt_generated/com/mobileappdevelopersclub/fapp/adapters/FinalsResponseAdapter$$ViewInjector.java
@@ -7,44 +7,44 @@ import butterknife.Views.Finder;
 public class FinalsResponseAdapter$$ViewInjector {
   public static void inject(Finder finder, final com.mobileappdevelopersclub.fapp.adapters.FinalsResponseAdapter target, Object source) {
     View view;
-    view = finder.findById(source, 2131296269);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296269' for field 'examDate' was not found. If this field binding is optional add '@Optional'.");
-    }
-    target.examDate = (android.widget.TextView) view;
-    view = finder.findById(source, 2131296270);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296270' for field 'examTime' was not found. If this field binding is optional add '@Optional'.");
-    }
-    target.examTime = (android.widget.TextView) view;
-    view = finder.findById(source, 2131296271);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296271' for field 'examLocation' was not found. If this field binding is optional add '@Optional'.");
-    }
-    target.examLocation = (android.widget.TextView) view;
-    view = finder.findById(source, 2131296257);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296257' for field 'className' was not found. If this field binding is optional add '@Optional'.");
-    }
-    target.className = (android.widget.TextView) view;
     view = finder.findById(source, 2131296272);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131296272' for field 'examInstructor' was not found. If this field binding is optional add '@Optional'.");
     }
     target.examInstructor = (android.widget.TextView) view;
+    view = finder.findById(source, 2131296271);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131296271' for field 'examLocation' was not found. If this field binding is optional add '@Optional'.");
+    }
+    target.examLocation = (android.widget.TextView) view;
+    view = finder.findById(source, 2131296270);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131296270' for field 'examTime' was not found. If this field binding is optional add '@Optional'.");
+    }
+    target.examTime = (android.widget.TextView) view;
+    view = finder.findById(source, 2131296269);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131296269' for field 'examDate' was not found. If this field binding is optional add '@Optional'.");
+    }
+    target.examDate = (android.widget.TextView) view;
     view = finder.findById(source, 2131296273);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131296273' for field 'classSection' was not found. If this field binding is optional add '@Optional'.");
     }
     target.classSection = (android.widget.TextView) view;
+    view = finder.findById(source, 2131296257);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131296257' for field 'className' was not found. If this field binding is optional add '@Optional'.");
+    }
+    target.className = (android.widget.TextView) view;
   }
 
   public static void reset(com.mobileappdevelopersclub.fapp.adapters.FinalsResponseAdapter target) {
-    target.examDate = null;
-    target.examTime = null;
-    target.examLocation = null;
-    target.className = null;
     target.examInstructor = null;
+    target.examLocation = null;
+    target.examTime = null;
+    target.examDate = null;
     target.classSection = null;
+    target.className = null;
   }
 }
