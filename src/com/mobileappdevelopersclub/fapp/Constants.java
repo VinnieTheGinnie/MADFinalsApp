79d6b51d012e44360200a5f4f7697ccca8720a3d
brianlindsy <blindsey@terpmail.umd.edu>
2014-04-29 18:20:21 -0400

    added constants for the GPA calculator

--- src/com/mobileappdevelopersclub/fapp/Constants.java
+++ src/com/mobileappdevelopersclub/fapp/Constants.java
@@ -39,6 +39,36 @@ public class Constants {
 	public static final int TRANS_OPTION = 5;
 	public static final int HEALTH_OPTION = 6;
 	public static final int EXTRAS_OPTION = 7;
+	public static final int GPA_OPTION = 8;
+	
+	//Used for the GPA calculator
+	public static final String A_PLUS = "A+";
+	public static final String A = "A";
+	public static final String A_MINUS = "A-";
+	public static final String B_PLUS = "B+";
+	public static final String B = "B";
+	public static final String B_MINUS = "B-";
+	public static final String C_PLUS = "C+";
+	public static final String C = "C";
+	public static final String C_MINUS = "C-";
+	public static final String D_PLUS = "D+";
+	public static final String D = "D";
+	public static final String D_MINUS = "D-";
+	public static final String F = "F";
+	public static final double A_PLUS_INT = 4.0;
+	public static final double A_INT = 4.0;
+	public static final double A_MINUS_INT = 3.7;
+	public static final double B_PLUS_INT = 3.3;
+	public static final double B_INT = 3.0;
+	public static final double B_MINUS_INT = 2.7;
+	public static final double C_PLUS_INT = 2.3;
+	public static final double C_INT = 2.0;
+	public static final double C_MINUS_INT = 1.7;
+	public static final double D_PLUS_INT = 1.3;
+	public static final double D_INT = 1.0;
+	public static final double D_MINUS_INT = .7;
+	public static final double F_INT = 0.0;
+
 	
 	//hardcoded image urls 
 	public static final String PIZZA_KINGDOM = "http://media-cdn.tripadvisor.com/media/photo-s/03/37/fc/fa/pizza-kingdom.jpg";
